56c6c2680d78c2fc0aa6bee43660192187a0fd53
Patrick Godschalk <patrick@kernelpanics.nl>
2016-01-20 21:59:23 +0100

    Bump year, some minor refactoring

--- Page.py
+++ Page.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014-2015 SwiperProxy Team
+# Copyright (c) 2014-2016 SwiperProxy Team
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -286,7 +286,7 @@ class Page(HtmlParser):
     def write_output(self, final):
         """
         Write output buffer in BLKSIZE blocks to the client. If final
-        is set, write everything, including the last block even if it 
+        is set, write everything, including the last block even if it
         is smaller than BLKSIZE. Remove written blocks from the output
         buffer.
         """

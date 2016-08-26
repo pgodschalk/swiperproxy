d7bcad03f4a97566d9688016a65af3352dc3f482
Patrick Godschalk <patrick@kernelpanics.nl>
2016-08-26 15:23:28 +0200

    Update http libs, bump copyright year

--- Util.py
+++ Util.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2014-2015 SwiperProxy Team
+# Copyright (c) 2014-2016 SwiperProxy Team
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
